4d0733e494a06d97792051148f4a94021c908418
Jack Baron <jackmbaron@gmail.com>
2021-10-28 17:06:27 +0100

    enable object api for Rust

--- flatc_run.sh
+++ flatc_run.sh
@@ -3,5 +3,5 @@ rm -rf ./generated
 
 flatc --java -I flatbuffer -o ./generated/java flatbuffer/WorldQLFB.fbs
 flatc --cpp -I flatbuffer -o ./generated/cpp flatbuffer/WorldQLFB.fbs
-flatc --rust -I flatbuffer -o ./generated/rust flatbuffer/WorldQLFB.fbs
+flatc --rust --gen-object-api -I flatbuffer -o ./generated/rust flatbuffer/WorldQLFB.fbs
 flatc --ts --gen-object-api -I flatbuffer -o ./generated/ts flatbuffer/WorldQLFB.fbs
