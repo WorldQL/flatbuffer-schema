ebca1c621ff72ffa4fa4e32b16adb43c49761863
Jack Baron <jackmbaron@gmail.com>
2021-10-28 17:06:57 +0100

    recompile

--- generated/ts/WorldQLFB.ts
+++
@@ -1,4 +0,0 @@
-export { Entity, EntityT } from './worldql-fb/messages/entity';
-export { Message, MessageT } from './worldql-fb/messages/message';
-export { Record, RecordT } from './worldql-fb/messages/record';
-export { Vec3d, Vec3dT } from './worldql-fb/messages/vec3d';
